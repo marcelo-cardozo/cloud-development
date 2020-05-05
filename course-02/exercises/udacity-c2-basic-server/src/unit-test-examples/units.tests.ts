fbab9010cf3d1e79493b469557ac6f1fadfa1bdd
Marcelo Cardozo <marcelo.cardozo@oktana.io>
2020-05-05 14:04:31 -0400

    unit test example implementations

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -35,6 +35,12 @@ describe('divide', () => {
 
 });
 
-// @TODO try creating a new describe block for the "concat" method
-// it should contain an it block for each it statement in the units.ts @TODO.
-// don't forget to import the method ;)
+describe('concat', ()=> {
+  it('should concatenate mar and celo', () => {
+    expect(concat("mar","celo")).to.equal("marcelo");
+  });
+
+  it('should throw error if parameter empty', ()=> {
+    expect(() => { concat("mar","") }).to.throw("parameters must not be empty");
+  });
+});
