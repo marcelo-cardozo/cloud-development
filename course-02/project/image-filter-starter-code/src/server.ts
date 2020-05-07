459acd0e0a0a1c08b6aafb2950ee7cac88deacc3
Marcelo Cardozo <marcelo.cardozo@oktana.io>
2020-05-07 12:15:19 -0400

    Add filter image endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,24 +13,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", 
+    async (req , res) => {
+      const imageUrl = req.query.image_url
+      console.log(imageUrl);
 
-  /**************************************************************************** */
+      if ( !imageUrl ){
+        return res.status(400).send('image_url required');
+      }else if(!isUrlValid(imageUrl)){
+        return res.status(400).send('image_url is not a valid url');
+      }
+      const filteredUrl = await filterImageFromURL(imageUrl);
+      
+      res.status(200).sendFile(filteredUrl);
+  });
 
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
@@ -43,4 +40,13 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
   } );
+
+  function isUrlValid(url : string) : boolean {
+    try {
+      new URL(url);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
 })();
