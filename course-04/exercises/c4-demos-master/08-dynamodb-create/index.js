af3694aeb7e14ebf8396889fa082da1c3e5e1943
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-05-20 13:29:57 -0400

    Chapter04 - add dynamodb create serverless function

--- course-04/exercises/c4-demos-master/08-dynamodb-create/index.js
+++ course-04/exercises/c4-demos-master/08-dynamodb-create/index.js
@@ -4,31 +4,29 @@ const AWS = require('aws-sdk')
 const uuid = require('uuid')
 
 const docClient = new AWS.DynamoDB.DocumentClient()
-const groupsTable = process.env.GROUPS_TABLE
+const groupsTable = process.env.GROUP_TABLE
 
 exports.handler = async (event) => {
-  console.log('Processing event: ', event)
-  const itemId = uuid.v4()
-
-  const parsedBody = JSON.parse(event.body)
-
-  const newItem = {
-    id: itemId,
+  const parsedBody = JSON.parse(event.body);
+  const item = {
+    id: uuid.v4(),
     ...parsedBody
   }
+  
+  var params = {
+    TableName : groupsTable,
+    Item: item
+  };
+  
+  await docClient.put(params).promise();
 
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise()
+  const response = {
+      statusCode: 201,
+      headers:{
+        "Access-Control-Allow-Origin":"*"
+      },
+      body: JSON.stringify(item),
+  };
+  return response;
+};
 
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      newItem
-    })
-  }
-}
