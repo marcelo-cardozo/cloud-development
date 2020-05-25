ee620f33e57fdb056daa34a170f7705fb89cef13
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-05-25 14:50:33 -0400

    0404-Generate signedurl to upload image to amazon s3

--- course-04/exercises/lesson-4/template-demo/src/lambda/http/createImage.ts
+++ course-04/exercises/lesson-4/template-demo/src/lambda/http/createImage.ts
@@ -3,8 +3,16 @@ import * as AWS from 'aws-sdk'
 import { v4 as uuidv4 } from 'uuid';
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+const s3 = new AWS.S3({
+    signatureVersion: 'v4' // Use Sigv4 algorithm
+})
+
 const groupTable = process.env.GROUPS_TABLE
 const imageTable = process.env.IMAGES_TABLE
+const imagesBucket = process.env.IMAGES_S3_BUCKET
+const signedExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+
+
 
 export const handler : APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> => {
     const groupId = event.pathParameters.groupId
@@ -23,10 +31,14 @@ export const handler : APIGatewayProxyHandler = async (event: APIGatewayProxyEve
     }
 
     const image = JSON.parse(event.body)
+    
+    const imageId = uuidv4()
+
     const newImage = {
-        imageId: uuidv4(),
+        imageId: imageId,
         groupId: groupId,
         timestamp: new Date().toISOString(),
+        imageUrl: `https://${imagesBucket}.s3.amazonaws.com/${imageId}`,
         ...image
     }
 
@@ -35,12 +47,16 @@ export const handler : APIGatewayProxyHandler = async (event: APIGatewayProxyEve
         Item: newImage
     }).promise()
 
+    const uploadUrl = getUploadUrl(imageId)
     return {
         statusCode: 201,
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify(newImage)
+        body: JSON.stringify({
+            newItem: newImage,
+            uploadUrl: uploadUrl
+        })
 
     }
 
@@ -55,4 +71,12 @@ async function groupExists(groupId: string) : Promise<Boolean>{
     }).promise()
 
     return result.Item != undefined
+}
+
+function getUploadUrl(imageId: string) : string {
+    return s3.getSignedUrl('putObject', {
+        Bucket: imagesBucket, // Name of an S3 bucket
+        Key: imageId, // id of an object this URL allows access to
+        Expires: signedExpiration  // A URL is only valid for . seconds
+     })
 }
